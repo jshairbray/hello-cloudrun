b43f2582ff45b960f792ef510df14506c698c4a8
Jonathan Shair <jshair@gmail.com>
2026-09-10 15:42:40 -0400

    Initial commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Cloud Run! 🎉"
+    return "Hello from Cloud Run and be kind! 🎉"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
